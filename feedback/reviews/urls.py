fe1fad4123d3bbb998f07b691a89452ca4830d2b
Yagiz Avsar <avsaryagiz@outlook.com>
2023-11-16 02:22:27 +0300

    function based view converted to class based

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.review),
+    path('', views.ReviewView.as_view()), # we update this part because we change our view from function based to class based 
     path('thank_you', views.thank_you)
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,28 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
-from .models import Review
+from django.views import View
+# from .models import Review
 
 # Create your views here.
 
 
-def review(request):
-    if request.method == 'POST':
-        existing_data = Review.objects.get(pk=1) # with model form method also we can update existing data like that
-        form = ReviewForm(request.POST, instance=existing_data)
-
-        if form.is_valid():
-            # review = Review(
-            #     user_name=form.cleaned_data['user_name'], review_text=form.cleaned_data['review_text'], rating=form.cleaned_data['rating'])
-            # review.save()
-            form.save() # now we can just save it with modelform no need extra connect to any model because its already connected a model
-            return HttpResponseRedirect('/thank_you')
-    else:
+class ReviewView(View):
+    def get(self, request):
         form = ReviewForm()
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
 
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
+    def post(self, request):
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/thank_you')
+
+
+# def review(request):
+#     if request.method == 'POST':
+#        # existing_data = Review.objects.get(pk=1) # with model form method also we can update existing data like that
+#         # form = ReviewForm(request.POST, instance=existing_data)
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             # review = Review(
+#             #     user_name=form.cleaned_data['user_name'], review_text=form.cleaned_data['review_text'], rating=form.cleaned_data['rating'])
+#             # review.save()
+#             form.save() # now we can just save it with modelform no need extra connect to any model because its already connected a model
+#             return HttpResponseRedirect('/thank_you')
+#     else:
+#         form = ReviewForm()
+
+#     return render(request, "reviews/review.html", {
+#         "form": form
+#     })
 
 
 def thank_you(request):
