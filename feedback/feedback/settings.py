b31565ba3f80631b9fa93182a6a1e0df6c306de8
Yagiz Avsar <avsaryagiz@outlook.com>
2023-11-17 03:10:37 +0300

    serving obtained image files practiced

--- feedback/feedback/settings.py
+++ feedback/feedback/settings.py
@@ -126,6 +126,7 @@ STATICFILES_DIRS = [
 
 
 MEDIA_ROOT= BASE_DIR / "uploads"
+MEDIA_URL = "/user-media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
